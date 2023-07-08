ae5941abe097ea8a7913d2aef9a1eb885d5ce1c9
Salah Ud Din <4yub1k@users.noreply.github.com>
2023-07-08 14:02:42 +0500

    fixes/Improvments

--- djHeadings/static/js/head.js
+++ djHeadings/static/js/head.js
@@ -1,5 +1,6 @@
 document.getElementById("id_mark").disabled = true
 
+document.getElementById("id_choice").addEventListener("click", check)
 function check() {
     let mark = document.getElementById("id_mark")
     if (document.getElementById("id_choice_2").checked){
@@ -13,7 +14,10 @@ function check() {
     }
 };
 
-
+const copy = document.getElementById("copy")
+if (copy) {
+    copy.addEventListener("click", copyText)
+}
 function copyText() {
     let htmlEditor = document.getElementById("outputHeading");
 
@@ -32,8 +36,33 @@ function copyText() {
     document.execCommand('copy')
     document.body.removeChild(container);
 
-    let  btn = document.getElementById("copy")
-    btn.textContent = "Copied !!!"
-    btn.style.backgroundColor= "yellow"
-    btn.style.color= "black"
+    function buttonUpdate(text, background, textColor){
+        let  btn = document.getElementById("copy")
+        btn.textContent = text
+        btn.style.backgroundColor= background
+        btn.style.color= textColor
+    }
+    buttonUpdate("Copied !!!", "yellow", "black")
+
+    setTimeout( () => {
+        buttonUpdate("Copy", "black", "white")
+    }, 4000)
 };
+
+
+const formValue = JSON.parse(document.getElementById('formDataDict').textContent);
+// console.log(formValue)
+document.getElementById("id_url").value = formValue.url? formValue.url: ''
+document.getElementById("id_mark").value = formValue.mark? formValue.mark: ''
+selectChoice(formValue.choice)
+
+function selectChoice(choice) {
+    // console.log(choice)
+    if ("ol" === choice){
+        document.getElementById("id_choice_0").checked = true
+    }else if ("ul" === choice){
+        document.getElementById("id_choice_1").checked = true
+    }else if ("p" === choice){
+        document.getElementById("id_choice_2").checked = true
+    }
+}
